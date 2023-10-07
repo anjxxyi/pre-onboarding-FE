9dae4355d815b4a99f3cfd3418e86bba00f52183
anjxxyi <anjxxyi@gmail.com>
2023-10-07 20:41:39 +0900

    feat: 로그인 페이지 구현

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import SigninForm from '../components/auth/SigninForm'
 
 const AuthPage = () => {
   return (
     <section className="page-auth">
+      <SigninForm />
     </section>
   )
 }
