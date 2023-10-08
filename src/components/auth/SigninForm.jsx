5095408dee10402118cda8b3ecf90a3d14aeabf5
anjxxyi <119813212+anjxxyi@users.noreply.github.com>
2023-10-08 20:38:01 +0900

    feat: 유저데이터를 통한 로그인 구현 (#1)
    # Part1. 로그인 개념 및 어플리케이션 구조
    
    1. username과 password를 입력하는 input과 submit button 생성
    2. users(유저데이터)와 _secret(암호키) 생성
    3. signinSubmitHandler : form에서 username과 password를 받아 signin 함수 호출
    4. signin : 올바른 username, password를 입력할 경우, {message: 'SUCCESS', token: (userInfo, _secret)} 반환
    5. getUserInfo : 위 signin 함수에서 token을 받아 유저 정보 반환
    6. 로그인을 성공할 경우, 하단에 user name(유저 정보)가 출력되도록 구현

--- src/components/auth/SigninForm.jsx
+++ src/components/auth/SigninForm.jsx
@@ -1,32 +1,83 @@
 import React, { useState } from 'react'
 
-const SigninForm = () => {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+const users = [
+  {
+    username: 'blue',
+    password: '1234',
+    userInfo: { name: 'blueStragglr' }
+  },
+  {
+    username: 'white',
+    password: '1234',
+    userInfo: { name: 'whiteDwarf' }
+  },
+  {
+    username: 'red',
+    password: '1234',
+    userInfo: { name: 'redGiant' }
+  }
+]
 
-  const handlerOnId = (e) => setId(e.target.value);
-  const handlerOnPw = (e) => setPw(e.target.value);
-  const handlerSubmit = (e) => {
+const _secret = '1234qwer!@#$';
+
+const signin = async (username, password) => {
+
+  const user = users.find((user) => {
+    return user.username === username && user.password === password;
+  })
+
+  return user ? {
+      messege: 'SUCCESS',
+      token: JSON.stringify({user: user.userInfo, secret: _secret})
+    } : null;
+}
+
+
+const getUserInfo = async (token) => {
+  const parsedToken = JSON.parse(token);
+  if (!parsedToken?.secret || parsedToken.secret !== _secret) return null;
+
+  const signinUser = users.find((user) => {
+    if (user.userInfo.name === parsedToken.user.name) return user;
+  });
+
+  return signinUser ? signinUser.userInfo : null;
+}
+
+const SigninForm = () => {
+  const [userInfo, setUserInfo] = useState(null);
+
+  const signinSubmitHandler = async (e) => {
     e.preventDefault();
-    if(!id || !pw) alert("Please enter your ID or PW.");
-    console.log('ID : ' + id);
-    console.log('PW : ' + pw);
+
+    const formData = new FormData(e.currentTarget);
+
+    const signinRes = await signin(formData.get('username'), formData.get('password'));
+    if (!signinRes) return alert("Please enter your username or password.");
+
+    const userInfo = await getUserInfo(signinRes.token);
+    if (!userInfo) return alert("Your information cannot be found.");
+
+    setUserInfo(userInfo);
+    alert(`Welcome, ‘${userInfo.name}’ 👋🏻`);
   }
 
   return (
     <article>
       <h2>Sign in</h2>
-      <form id="form" onSubmit={handlerSubmit}>
+      <form id="form" onSubmit={signinSubmitHandler}>
         <div className="form-group">
-          <label>ID</label>
-          <input type="text" name="id" value={id} onChange={handlerOnId} placeholder="Enter ID" />
+          <label htmlFor="username">ID</label>
+          <input type="text" name="username" placeholder="Enter ID" />
         </div>
         <div className="form-group">
-          <label>PW</label>
-          <input type="password" name="password" autoComplete="on" value={pw} onChange={handlerOnPw} placeholder="Enter Password" />
+          <label htmlFor="password">PW</label>
+          <input type="password" name="password" autoComplete="off" placeholder="Enter PW" />
         </div>
         <button type="submit">Submit</button>
       </form>
+      <br />
+      <p>{userInfo ? `user name : ${userInfo.name}` : `Please sign in ;(`}</p>
     </article>
   )
 }
